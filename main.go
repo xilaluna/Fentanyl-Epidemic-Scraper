6b912577b621019ded923268a44df33269443f2a
xiluna <xilaluna2@gmail.com>
2022-02-27 02:46:13 -0800

    [add] working title check

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"os"
 	"strconv"
-	"strings"
 
 	"github.com/gocolly/colly"
 )
@@ -17,27 +14,27 @@ type Article struct {
 }
 
 func main() {
+	// articles := []Article{}
+
 	// Instantiate default collector
 	collector := colly.NewCollector(
 		colly.AllowedDomains("darknetlive.com"),
 	)
 
-	articles := []Article{}
+	articleCollector := collector.Clone()
 
 	// On every a article grab the title and date
-	collector.OnHTML(".a1e", func(e *colly.HTMLElement) {
-		title := e.ChildText(".a1f")
-		date := e.ChildText(".a1g > time")
-		if strings.Contains(strings.ToLower(title), "fent") {
-			newArticle := Article{}
-
-			newArticle.Title = title
-			newArticle.Date = date
-
-			articles = append(articles, newArticle)
-		}
-		
+	collector.OnHTML("article.a1e > a", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		link_title := e.Attr("title")
+		fmt.Println(link_title)
+		articleCollector.Visit(e.Request.AbsoluteURL(link))
 	}) 
+
+	articleCollector.OnHTML(".h-entry", func(e *colly.HTMLElement) {
+		title := e.ChildText(".a1i")
+		fmt.Printf("%v", title)
+	})
 
 	// Before making a request print "Visiting ..."
 	collector.OnRequest(func(r *colly.Request) {
@@ -50,15 +47,15 @@ func main() {
 
 	// Start scraping url and its numbered pages
 	collector.Visit("https://darknetlive.com/post/")
-	for i := 2; i < 20; i++ {
+	for i := 2; i < 3; i++ {
 		collector.Visit("https://darknetlive.com/post/page/" + strconv.Itoa(i) + "/")
 	}
 
-	json, _ := json.MarshalIndent(articles, "", "  ")
+	// json, _ := json.MarshalIndent(articles, "", "  ")
 
-	writeError := os.WriteFile("data.json", json, 0644)
-	if writeError != nil {
-		panic(writeError)
-	}
+	// writeError := os.WriteFile("data.json", json, 0644)
+	// if writeError != nil {
+	// 	panic(writeError)
+	// }
 
 }
